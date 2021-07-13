dc502102a7eb7f5160878a378e1c1ee3f88b7419
mong_head <camara1melon315@gmail.com>
2021-07-13 09:13:32 +0900

    restful get method prac

--- helloweb-ex02/routes/user.js
+++ helloweb-ex02/routes/user.js
@@ -2,6 +2,12 @@ const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
+router.route("/:no").get(function(req,res) {
+    res.render("user/info",{
+        no: req.params.no || 0
+    });
+});
+
 router.route("/join").get(function(req,res) {
     res.render("user/join");
 });
