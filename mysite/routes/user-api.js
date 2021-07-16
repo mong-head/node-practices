0a0d33ca69067982010e2522fa12695858539493
mong_head <camara1melon315@gmail.com>
2021-07-16 10:31:17 +0900

    user-api : error

--- mysite/routes/user-api.js
+++ mysite/routes/user-api.js
@@ -5,5 +5,12 @@ const auth = require('./auth');
 const router = express.Router()
 router.route('/checkemail').get(controller.checkemail);
 router.route('/needauth').get(auth,(req,res)=> res.send({result:"success"}));
+router.route('/error').get((req,res,next)=> {
+    try{
+        throw new Error('Broken');
+    } catch(err) {
+        next(err);
+    }
+});
 
 module.exports = router;
