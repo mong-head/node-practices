d9ac8bbce7fa2dec83614c00a00283cc00b2fc31
mong_head <camara1melon315@gmail.com>
2021-07-16 11:41:05 +0900

    guestbook-api : delete, read #1

--- mysite/controllers/guestbook-api.js
+++ mysite/controllers/guestbook-api.js
@@ -1,20 +1,47 @@
 
 module.exports = {
     read: async (req,res,next) => {
-
+        const startNo = req.query.sno || 0;
+        console.log(startNo);
+        // sql select(limit) - do this
+        res.status(200).send({
+            result: "success",
+            data: [{
+                no: startNo-1,
+                name: '둘리',
+                message: '호이~~',
+                reg_date: new Date()
+            },{
+                no: startNo-2,
+                name: '둘리2',
+                message: '호이2~~',
+                reg_date: new Date()
+            },{
+                no: startNo-3,
+                name: '둘리3',
+                message: '호이3~~',
+                reg_date: new Date()
+            }],
+            message: null
+        })
     },
-    delete: async (req,res,next) =>{
-
+    delete: async (req,res,next) => {
+        console.log(req.params.no + ":" + req.body.password);
+        // sql delete - do this
+        res.status(200).send({
+            result: 'success',
+            data: req.params.no,
+            message: null
+        });
     },
     post: async (req,res,next) =>{
-        console.log(req.body);
         // sql insert - do this
         res.status(200).send({
             result: 'success',
             data: Object.assign(req.body,{
                 no: 10,
                 password: '', //password 가리기
-                regDate: new Date() // now
+                reg_date: new Date() // now
             }),
             message: null
         });
--- mysite/routes/guestbook-api.js
+++ mysite/routes/guestbook-api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controller = require('../controllers/guestbook-api');
 
 const router = express.Router();
-router.route("/:no").get(controller.read);
+router.route("").get(controller.read);
 router.route("/:no").delete(controller.delete);
 router.route("").post(controller.post);
 
