ad2774109f67362fc5e70d93f6d2f14c7e54feea
mong_head <camara1melon315@gmail.com>
2021-07-20 18:44:59 +0900

    feat(board) : auth

--- mysite/routes/board.js
+++ mysite/routes/board.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const controller = require('../controllers/board')
+const controller = require('../controllers/board');
+const auth = require('./auth');
 
 const router = express.Router();
 router.route("/view/:no").get(controller.view);
-router.route("/write").get(controller.write);
-router.route("/write(/:no)?").post(controller._write);
-router.route("/update/:no").get(controller.update);
-router.route("/update/:no").post(controller._update);
-router.route("/delete/:no").get(controller.delete);
+router.route("/write").get(auth(),controller.write);
+router.route("/write(/:no)?").post(auth(),controller._write);
+router.route("/update/:no").get(auth(),controller.update);
+router.route("/update/:no").post(auth(),controller._update);
+router.route("/delete/:no").get(auth(),controller.delete);
 router.route("(/:no)?").get(controller.index);
 router.route("(/:no)?").post(controller.index);
 
